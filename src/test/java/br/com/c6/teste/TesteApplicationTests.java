f02e15a1983a99f905096476a2fc75389192ab67
Rodolfo da Costa Lacerda <rodolfo.lacerda-ext@ab-inbev.com>
2023-07-27 16:45:04 -0300

    Configuration commit

---
+++ src/test/java/br/com/c6/teste/TesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.c6.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
